fc894571a845dc8413bc2fc28674688573b001af
ipavez <ipavezurbina@gmail.com>
2024-12-26 19:39:35 -0300

    mongo

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-
+import userModel from '../models/user.model.js';
 const router = Router();
 
 
-router.get('/register', (req, res) => {
-    res.render('register');
+router.post('/register', async(req, res) => {
+    const { name, email, password } = req.body;
+    const exists = await userModel.find({email : email}).exec();
+    console.log(exists)
+    if (exists.length > 0) {
+        return res.status(406).send({ status: 'error', error: 'User already exists' });
+    }
+    const user = userModel.create({ user_name : name , email : email, password : password });
+    res.status(201).send({ status: 'ok', user });
 })
 
 
